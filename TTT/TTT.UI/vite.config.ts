60f0b0f95da11a868266577c838873a612eae12b
NightOwl <leonmerk@gmx.de>
2022-07-31 18:49:45 +0200

    Update vite.config.ts

--- TTT/TTT.UI/vite.config.ts
+++ TTT/TTT.UI/vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
   build: {
-    outDir: '../../Server/resources/roleplay/client/view'
+    outDir: '../../Server/resources/roleplay/client/view',
+    emptyOutDir: true,
   },
   plugins: [react()]
 })
